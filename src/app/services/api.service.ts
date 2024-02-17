4269f43c9886ea75d3da8a5d3ff1a8da02aba53d
whit33y <troppout@gmail.com>
2024-02-17 18:29:42 +0100

    get request with services #5

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Options } from '../../types';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    //httpClient jest w paczce angular/common i pozwala nam wysyłać api call'sy do wybranych url'ów np. GET request
+  }
+  get<T>(url: string, options: Options): Observable<T> {
+    return this.httpClient.get<T>(url, options) as Observable<T>;
+  }
 }
