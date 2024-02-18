6f8d4684ed4a1026c0cad2af2169d44d1490fe54
whit33y <troppout@gmail.com>
2024-02-18 18:59:12 +0100

    Wrapper, import, and basic styles added #7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'tutorial';
