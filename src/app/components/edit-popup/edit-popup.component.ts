8f145b3cd3203842c075934d3ae2e0a9303c04f9
whit33y <troppout@gmail.com>
2024-02-20 17:05:35 +0100

    Inputs, Outputs created for edit popup #23

--- src/app/components/edit-popup/edit-popup.component.ts
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import { Product } from '../../../types';
 
 @Component({
   selector: 'app-edit-popup',
   standalone: true,
-  imports: [],
+  imports: [DialogModule, CommonModule],
   templateUrl: './edit-popup.component.html',
-  styleUrl: './edit-popup.component.scss'
+  styleUrl: './edit-popup.component.scss',
 })
 export class EditPopupComponent {
+  @Input() display: boolean = false;
+  @Output() confirm = new EventEmitter<Product>();
 
+  @Input() product: Product = {
+    name: '',
+    image: '',
+    price: '',
+    rating: 0,
+  };
+
+  onConfirm() {
+    this.confirm.emit(this.product);
+  }
+
+  onCancel() {
+    this.display = false;
+  }
 }
