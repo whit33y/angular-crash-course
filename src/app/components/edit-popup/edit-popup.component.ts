c82f3205ef3f1364786fd681a8c8968ee3ae13df
whit33y <troppout@gmail.com>
2024-02-21 13:29:53 +0100

    Edit popup form created #24

--- src/app/components/edit-popup/edit-popup.component.ts
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -2,24 +2,26 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { Product } from '../../../types';
+import { FormsModule } from '@angular/forms';
+import { RatingModule } from 'primeng/rating';
 
 @Component({
   selector: 'app-edit-popup',
   standalone: true,
-  imports: [DialogModule, CommonModule],
+  imports: [DialogModule, CommonModule, FormsModule, RatingModule],
   templateUrl: './edit-popup.component.html',
   styleUrl: './edit-popup.component.scss',
 })
 export class EditPopupComponent {
   @Input() display: boolean = false;
-  @Output() confirm = new EventEmitter<Product>();
-
+  @Input() header: string = '';
   @Input() product: Product = {
     name: '',
     image: '',
     price: '',
     rating: 0,
   };
+  @Output() confirm = new EventEmitter<Product>();
 
   onConfirm() {
     this.confirm.emit(this.product);
