718ccf8b9f5543814f31d46c97a4a6164e2d144f
whit33y <troppout@gmail.com>
2024-02-21 15:32:00 +0100

    Delete confirmation popup added #27

--- src/app/components/edit-popup/edit-popup.component.ts
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -20,6 +20,7 @@ import { ButtonModule } from 'primeng/button';
 })
 export class EditPopupComponent {
   @Input() display: boolean = false;
+  @Output() displayChange = new EventEmitter<boolean>();
   @Input() header!: string;
   @Input() product: Product = {
     name: '',
@@ -31,9 +32,12 @@ export class EditPopupComponent {
 
   onConfirm() {
     this.confirm.emit(this.product);
+    this.display = false;
+    this.displayChange.emit(this.display);
   }
 
   onCancel() {
     this.display = false;
+    this.displayChange.emit(this.display);
   }
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -3,19 +3,32 @@ import { Product } from '../../../types';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { ConfirmationService } from 'primeng/api';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [RatingModule, FormsModule, ButtonModule],
+  imports: [RatingModule, FormsModule, ButtonModule, ConfirmPopupModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
+  providers: [ConfirmationService],
 })
 export class ProductComponent {
+  constructor(private confirmationService: ConfirmationService) {}
   @Input() product!: Product;
   @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
   @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
+
   editProduct() {
     this.edit.emit(this.product);
+  }
+  confirmDelete() {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete this product?',
+      accept: () => {
+        this.deleteProduct();
+      },
+    });
   }
 
   deleteProduct() {
