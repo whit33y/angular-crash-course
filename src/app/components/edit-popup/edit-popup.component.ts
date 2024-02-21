443e9d05144d16407d17ffd2579b5e2211505bf3
whit33y <troppout@gmail.com>
2024-02-21 14:00:23 +0100

    Outputs, functions, buttons added  #26

--- src/app/components/edit-popup/edit-popup.component.ts
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -4,17 +4,23 @@ import { DialogModule } from 'primeng/dialog';
 import { Product } from '../../../types';
 import { FormsModule } from '@angular/forms';
 import { RatingModule } from 'primeng/rating';
-
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-edit-popup',
   standalone: true,
-  imports: [DialogModule, CommonModule, FormsModule, RatingModule],
+  imports: [
+    DialogModule,
+    CommonModule,
+    FormsModule,
+    RatingModule,
+    ButtonModule,
+  ],
   templateUrl: './edit-popup.component.html',
   styleUrl: './edit-popup.component.scss',
 })
 export class EditPopupComponent {
   @Input() display: boolean = false;
-  @Input() header: string = '';
+  @Input() header!: string;
   @Input() product: Product = {
     name: '',
     image: '',
